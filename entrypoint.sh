c94c699baed75bce3466546d8bfd0d4de1a7b270
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-10 13:56:54 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ echo "First argument: $1"
 echo "MEESHKAN_ENDPOINT: $MEESHKAN_ENDPOINT"
 echo "MEESHKAN_TEST: $MEESHKAN_TEST"
 
+# See https://github.community/t5/GitHub-Actions/Docker-networking-inside-a-job/td-p/34621#
+# and the there referenced https://github.com/chrispat/next-aws-lambda/commit/5f72ecb96dca8275773af0392f5dcbf2f5d5627f
+# and https://stackoverflow.com/questions/31324981/how-to-access-host-port-from-docker-container
 hostip=$(ip route show | awk '/default/ {print $3}')
 export BASE_URL="http://${hostip}:8080/"
 
