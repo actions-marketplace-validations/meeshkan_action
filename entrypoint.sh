932b5eb0434ca809b60759d9d3f08554fd0293c4
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-10 13:35:55 +0200

    Use curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -l
 
+time=$(date)
+echo ::set-output name=meeshkan_time::$time
+
 echo "First argument: $1"
 echo "MEESHKAN_ENDPOINT: $MEESHKAN_ENDPOINT"
 echo "MEESHKAN_TEST: $MEESHKAN_TEST"
 
-time=$(date)
-echo ::set-output name=meeshkan_time::$time
+echo "Trying to access entrypoint: $MEESHKAN_ENDPOINT"
+curl $MEESHKAN_ENDPOINT
+
 
