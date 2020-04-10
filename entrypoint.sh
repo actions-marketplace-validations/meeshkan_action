19ec47837895c938f33d7375b0aefb4fbbe547c2
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-10 13:52:14 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,10 @@ echo "First argument: $1"
 echo "MEESHKAN_ENDPOINT: $MEESHKAN_ENDPOINT"
 echo "MEESHKAN_TEST: $MEESHKAN_TEST"
 
-echo "Trying to access entrypoint: $MEESHKAN_ENDPOINT"
-curl $MEESHKAN_ENDPOINT
+hostip=$(ip route show | awk '/default/ {print $3}')
+export BASE_URL="http://${hostip}:8080/"
+
+echo "Trying to access entrypoint: $BASE_URL"
+curl $BASE_URL
 
 
