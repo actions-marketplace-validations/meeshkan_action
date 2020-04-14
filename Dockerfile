468c285509a84964f6aa37ea4f139a31e6679bf5
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-14 10:18:51 +0200

    Switch to schemathesis base image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM alpine:3.10
+FROM kiwicom/schemathesis:stable
 
-RUN apk add --update curl \
-    && rm -rf /var/cache/apk/*
+# FROM alpine:3.10
+# RUN apk add --update curl \
+#     && rm -rf /var/cache/apk/*
 
 COPY entrypoint.sh /entrypoint.sh
-
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,20 @@
 time=$(date)
 echo ::set-output name=meeshkan_time::$time
 
-echo "First argument: $1"
-echo "MEESHKAN_ENDPOINT: $MEESHKAN_ENDPOINT"
-echo "MEESHKAN_TEST: $MEESHKAN_TEST"
+# echo "First argument: $1"
+# echo "MEESHKAN_ENDPOINT: $MEESHKAN_ENDPOINT"
+# echo "MEESHKAN_TEST: $MEESHKAN_TEST"
 
 # See https://github.community/t5/GitHub-Actions/Docker-networking-inside-a-job/td-p/34621#
 # and the there referenced https://github.com/chrispat/next-aws-lambda/commit/5f72ecb96dca8275773af0392f5dcbf2f5d5627f
 # and https://stackoverflow.com/questions/31324981/how-to-access-host-port-from-docker-container
-hostip=$(ip route show | awk '/default/ {print $3}')
-export BASE_URL="http://${hostip}:8080/"
+HOST_IP=$(ip route show | awk '/default/ {print $3}')
+REWRITTEN_ENDPOINT=$(echo $MEESHKAN_ENDPOINT | sed "s/\/\/localhost/\/\/$HOST_IP/")
 
-echo "Trying to access entrypoint: $BASE_URL"
-curl $BASE_URL
+echo "Original endpoint: $MEESHKAN_ENDPOINT"
+echo "Rewritten endpoint: $REWRITTEN_ENDPOINT"
+
+# curl $BASE_URL
+schemathesis $REWRITTEN_ENDPOINT
 
 
