221a52a741fb47d79bdca4169f4cbe3c759ff8ad
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-27 10:59:26 +0100

    Update logging

--- entrypoint.py
+++ entrypoint.py
@@ -4,7 +4,7 @@ import sys
 import os
 import requests
 
-print("Meeshkan GitHub Action v0.8")
+print("Meeshkan GitHub Action v0.9")
 
 TESTER_ENDPOINT_URL = (
     "https://txwwxrn43i.execute-api.eu-west-1.amazonaws.com/prod/test-trigger"
@@ -36,9 +36,11 @@ def run_tests(client_id, client_secret, url):
         if response.status_code == 200:
             print("Triggered test run")
         else:
-            sys.exit(f"Internal error trigger tests: {response.status_code}")
+            print(f"Internal error trigger tests: {response.status_code}")
+            sys.exit(1)
     except Exception:
-        sys.exit(f"Invalid URL specified: {url}")
+        print(f"Invalid URL specified: {url}")
+        sys.exit(1)
 
 
 run_tests(MEESHKAN_CLIENT_ID, MEESHKAN_CLIENT_SECRET, MEESHKAN_URL)
